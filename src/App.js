2b21e1666be53a3edaa32b4f39686fd13e30e078
Norman Pendzich <73986750+NormanPen@users.noreply.github.com>
2022-04-04 14:31:43 +0200

    Alle nötigen Abhängigkeiten hinzugefügt

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
+
+import Home from "./sites/Home";
+import Imprint from "./sites/Imprint";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <Switch>
+          <Route path="/" exact><Home/></Route>
+            <Route path="/impressum"><Imprint></Imprint></Route>
+        </Switch>
+      </Router>
+
   );
 }
 
---
+++ src/sites/Imprint/index.js
@@ -0,0 +1,9 @@
+const Imprint =()=>{
+    return(
+        <div>
+            <h1>Impressum</h1>
+        </div>
+    );
+}
+
+export default Imprint;
