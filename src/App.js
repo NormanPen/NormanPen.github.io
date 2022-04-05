6e885d03b3f4b9620f8503e03c24a4b3b168d748
Norman Pendzich <73986750+NormanPen@users.noreply.github.com>
2022-04-05 17:54:38 +0200

    Arbeiten an der Nav und am Header

--- src/App.js
+++ src/App.js
@@ -1,19 +1,40 @@
+import { useState } from "react";
 import { HashRouter as Router, Switch, Route } from "react-router-dom";
 
+import Navigation from "./components/Navigation";
 import Home from "./sites/Home";
 import Imprint from "./sites/Imprint";
 
+const App = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
 
-function App() {
+  const [isDay, setIsDay] = useState(false);
+  const toggleDayNight = () => {
+    setIsDay(!isDay);
+  };
   return (
+    <div className={`relative ${isDay ? "dark" : ""}`}>
       <Router>
+        <Navigation
+          isOpen={isOpen}
+          toggle={toggle}
+          isDay={isDay}
+          toggleDayNight={toggleDayNight}
+        />
         <Switch>
-          <Route path="/" exact><Home/></Route>
-            <Route path="/impressum"><Imprint></Imprint></Route>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+          <Route path="/impressum">
+            <Imprint />
+          </Route>
         </Switch>
       </Router>
-
+    </div>
   );
-}
+};
 
 export default App;
--- src/sites/Home/index.js
+++ src/sites/Home/index.js
@@ -1,11 +1,11 @@
-const Home =()=>{
-    return(
-        <div>
-            <h1 className="text-3xl font-bold underline">
-                Hello world!
-            </h1>
-        </div>
-    );
-}
+import Header from "../../components/Header";
+
+const Home = () => {
+  return (
+    <div className="h-80">
+      <Header></Header>
+    </div>
+  );
+};
 
 export default Home;
